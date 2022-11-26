79480f5af0a18127465400582c496a89b47ab10b
Alexander <aurakhovtel@gmail.com>
2022-11-27 00:20:48 +0500

    Initial commit

--- src/LR1/Example4.java
+++ src/LR1/Example4.java
@@ -1,2 +1,18 @@
-package LR1;public class Example4 {
+package LR1;
+
+import java.util.Scanner;
+
+public class Example4 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input number");
+        int num = in.nextInt();
+
+        System.out.println("Your number: " + num);
+        in.close();
+    }
+
 }
