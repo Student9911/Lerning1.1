9a454d1781cf1bb97de3369ab7e935d7d5be6c25
aurakhov <aurakhovtel@gmail.com>
2022-12-08 11:26:03 +0500

    LR3

--- src/LR3/Example3.java
+++ src/LR3/Example3.java
@@ -9,9 +9,11 @@ public class Example3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n;
-        System.out.println("Введите количество итераций:");
+        System.out.println("Введите количество чисел в последовательности:");
         n = in.nextInt();
         int a = 1, b = 1;
+        System.out.println("Сейчас будет выведено число Фибонначи используя операцию цикла while,\n" +
+                "количество чисел в последовательности введено пользователем");
         System.out.print(a + ", " + b);
         int c, i = 2;
         while (i < n) {
@@ -20,6 +22,18 @@ public class Example3 {
             b = c;
             System.out.print(", " + c);
             i++;
+        }
+        System.out.println("\nСейчас будет выведено число Фибонначи используя операцию цикла for,\n" +
+                "количество чисел в последовательности введено также пользователем");
+        a = 1;
+        b = 1;
+        System.out.print(a + ", " + b);
+        for (i = 0; i < n - 2; i ++) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.print(", " + c);
+
         }
     }
 }
