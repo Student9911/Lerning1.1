24856a741776d513ad55f0107c57d2c309236ade
aurakhov <aurakhovtel@gmail.com>
2022-12-08 11:03:11 +0500

    LR3

--- src/LR3/Example8.java
+++ src/LR3/Example8.java
@@ -1,42 +1,34 @@
 package LR3;
 
-import java.util.Scanner;
-
 public class Example8 {
 
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        char[] alf = new char[10];
-        int[] ints = new int[alf.length];
+        int n = 10;
+        char[] chars = new char[n];
+        char[] onlyChars = new char[] {'A', 'E', 'I', 'O', 'U', 'Y'};
+        char b = 'A';
         int i;
-        int z;
-        int x = 67;
-        z = x;
-        for (i = 0; i < alf.length; i++) {
-            z ++;
-            if (z == 70 || z == 74 || z == 80 || z == 90) {
+        int j;
 
-                i --;
 
-            } else ;
-            int y = 0;
-            y ++;
-            ints[i] = x;
-            x ++;
-            alf[y] = (char) ints[y];
-            System.out.println(x + " " + alf[i]);;
-
+        for(i = 0; i < n; i++) {
+            int q = 1;
+            for(j = 0; j < onlyChars.length; j++) {
+                if(b == onlyChars[j]) {
+                    i--;
+                    q = 0;
+                }
+            }
+            if(q == 1) {
+                chars[i] = b;
+            }
+            b++;
         }
-
-
-
-
-
-
+        for(int k = 0; k < n; k++){
+            System.out.print(chars[k] + ", ");
+        }
     }
 }
-
         // 8. Напишите программу, в которой создается символьный массив
         // из 10 элементов. Массив заполнить большими (прописными) буквами
         // английского алфавита. Буквы берутся подряд, но только согласные
