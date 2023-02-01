2cd3c0033ee6f70d1436ae0c406e5110fbc06d48
aurakhov <aurakhovtel@gmail.com>
2023-02-01 23:45:35 +0500

    Timus

---
+++ src/timus/task_1243.java
@@ -0,0 +1,14 @@
+package timus;
+
+import java.util.Scanner;
+
+public class task_1243 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String out = in.nextLine();
+        long n = Integer.parseInt(out);
+        System.out.println(n % 7);
+    }
+}
