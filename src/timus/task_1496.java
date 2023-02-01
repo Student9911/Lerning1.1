0a0d043f301956540c335e46ea50fabc1787e35f
aurakhov <aurakhovtel@gmail.com>
2023-02-01 22:29:53 +0500

    Timus

---
+++ src/timus/task_1496.java
@@ -0,0 +1,41 @@
+package timus;
+
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class task_1496 {
+
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String[] submit = new String[n];
+        int count;
+        HashSet<String> spammers = new HashSet<String>();
+        for (int i = 0; i < submit.length; i++) {
+            submit[i] = in.next();
+            spammers.add(submit[i]);
+        }
+
+        for (String t : spammers) {
+            count = 0;
+            for (int i = 0; i < submit.length; i++) {
+                if (t.equals(submit[i])) {
+                    count++;
+                }
+            }
+            if (count > 1) {
+                System.out.println(t);
+
+            }
+
+        }
+
+
+
+    }
+}
+
+
