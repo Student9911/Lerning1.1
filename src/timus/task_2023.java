e4a1a6068b46e8825411e4e375f662d6fe249658
aurakhov <aurakhovtel@gmail.com>
2023-02-02 23:28:54 +0500

    Timus

--- src/timus/task_2023.java
+++ src/timus/task_2023.java
@@ -1,29 +1,28 @@
 package timus;
 
 import java.util.Scanner;
-
-public class task_2023_ {
+public class task_2023 {
 
     public static void main(String[] args) {
 
 
         String[][] name = new String[][]{{"Alice", "Ariel", "Aurora", "Phil", "Peter", "Olaf", "Phoebus", "Ralph", "Robin"},
-                {"Bambi", "Belle", "Bolt", "Mulan", "Movgli", "Mickey", "Silver", "Simba", "Stitch"},
+                {"Bambi", "Belle", "Bolt", "Mulan", "Mowgli", "Mickey", "Silver", "Simba", "Stitch"},
                 {"Dumbo", "Genie", "Jiminy", "Kuzko", "Kida", "Kenai", "Tarzan", "Tiana", "Winnie"}};
 
         Scanner in = new Scanner(System.in);
-        int n = Integer.parseInt(in.nextLine());
+        int n = in.nextInt();
         int curent_position = 0;
         int steps = 0;
         String a;
 
 
         for (int i = 0; i < n; i ++) {
-            a = in.nextLine();
+            a = in.next();
             for (int j =0; j < name.length; j ++) {
                 for (int k = 0; k < 9; k ++) {
-                    if (a.equals(name[k])) {
-                        steps += Math.abs( - curent_position);
+                    if (a.equals(name[j][k])) {
+                        steps += Math.abs(j - curent_position);
                         curent_position = j;
                     }
 
