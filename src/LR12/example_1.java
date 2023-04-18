ed8476eea63a48ceff88a13428a36804cb1d64c0
aurakhov <aurakhovtel@gmail.com>
2023-04-18 22:23:22 +0500

    LR12

--- src/LR12/example_1.java
+++ src/LR12/example_1.java
@@ -9,14 +9,7 @@ public class example_1 {
     }
 
     public static int[] getEvenNumbers(int[] arr) {
-        int[] evenArr = new int[arr.length];
-        int index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
-                evenArr[index] = arr[i];
-                index++;
-            }
-        }
-        return Arrays.copyOf(evenArr, index);
+
+        return Arrays.stream(arr).filter(x -> x % 2 == 0).toArray();
     }
 }
