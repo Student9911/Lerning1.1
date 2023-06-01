60b6b7f1fbebfa5d1f9c2947632ebc5adc79a5fc
aurakhov <aurakhovtel@gmail.com>
2023-06-02 00:34:53 +0500

    LR13

---
+++ src/LR13/example2.java
@@ -0,0 +1,25 @@
+package LR13;
+
+import java.time.LocalTime;
+
+public class example2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i <= 10; i ++) {
+                System.out.println(i);
+
+            }
+            try {
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        t1.start();
+        t1.join();
+    }
+
+}
